097cdfa6a54f07e61a78ae8866ed8d7557605f7f
stendler <m.stendler@fu-berlin.de>
2020-07-18 22:04:37 +0200

    update setup with split indiv dirs and install their dependencies
    Signed-off-by: stendler <m.stendler@fu-berlin.de>

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
+echo "➡ Setting up virtual env"
 python3 -m venv venv
 source venv/bin/activate
 
-pip install -r indiv-scores/requirements.txt
+echo "➡ Installing function dependencies"
+pip install -r indiv-performancescores/requirements.txt
+pip install -r indiv-driverscores/requirements.txt
 pip install -r driver-scores/requirements.txt
 pip install -r performance-scores/requirements.txt
 
